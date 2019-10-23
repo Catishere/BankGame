64af9cf20a92204eaa9b3a4077f5ac281289cecd
Catishere <viktor.gyoshev@gmail.com>
2019-10-23 09:24:41 +0300

    Added Security Manager forbidding usage of reflect.

--- src/main/Main.java
+++ src/main/Main.java
@@ -2,10 +2,18 @@ package main;
 
 import banking.Bank;
 import model.User;
+import security.ReflectionSecurityManager;
 
 public class Main {
 
     public static void main(String[] args) {
+
+        try {
+            System.setSecurityManager(new ReflectionSecurityManager());
+        } catch (SecurityException se) {
+            System.out.println("Double call.");
+        }
+
         User user = Bank.getInstance().openAccount("Wiki", "parola", 50.0);
         User user2 = Bank.getInstance().openAccount("Bobi", "parol1a", 30.0);
         User user3 = Bank.getInstance().openAccount("Niki", "parola2", 510.0);
---
+++ src/security/ReflectionSecurityManager.java
@@ -0,0 +1,14 @@
+package security;
+
+public class ReflectionSecurityManager extends SecurityManager {
+
+    public ReflectionSecurityManager() {
+    }
+
+    @Override
+    public void checkPackageAccess(String pkg){
+        if(pkg.equals("java.lang.reflect")){
+            throw new SecurityException("Illegal package: java.lang.reflect not allowed!");
+        }
+    }
+}
